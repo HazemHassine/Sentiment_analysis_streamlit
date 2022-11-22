edfc6f8cc740fca774c727aeaa90a5ed281b5af8
hazem hassine <70240216+HazemHassine@users.noreply.github.com>
2022-11-22 19:36:52 +0100

    added the tokenizer to be loaded automatically instead of reinstanciating it everytime

--- app.py
+++ app.py
@@ -3,31 +3,18 @@ import streamlit as st
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import json
+import re
 
-vocab_size = 10000
-embedding_dim = 16
 max_length = 100
 trunc_type='post'
 padding_type='post'
-oov_tok = "<OOV>"
-training_size = 20000
 
 with open("model_pkl.pkl", "rb") as f:
     model = pkl.load(f)
 
-with open("./sarcasm.json", 'r') as f:
-    datastore = json.load(f)
 
-sentences = []
-
-for item in datastore:
-    sentences.append(item['headline'])
-
-training_sentences = sentences[0:training_size]
-
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-tokenizer.fit_on_texts(training_sentences)
-
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pkl.load(f)
 
 def predict(model , sentences: list) -> None:
     sentences = tokenizer.texts_to_sequences(sentences)
@@ -36,11 +23,9 @@ def predict(model , sentences: list) -> None:
 
 st.title("Sentiment Analysis 🎭 for news headers")
 st.info("Enter a news header to analyse it's mood (sarcastic/not sarcastic)")
-sentence = st.text_input("Your sentence")  
+sentence = st.text_input("Your sentence")
+res_pattern = re.compile(r"\d\.\d\d")
 result = predict(model, [sentence])
 
-result = str(100 - float(str(result).split("[")[2].split("]")[0][:3])*100) + "%" + " Sarcastic"
-st.text(result)
 
-
-print("good")
+st.text(f"{float(re.findall(res_pattern, str(result))[0])*100} % sarcastic")
